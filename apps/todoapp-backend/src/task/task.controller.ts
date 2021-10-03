51a5ccf54247b968600dbac72c3695d44d6c2b89
ZERO-94 <kimanh.tvka@gmail.com>
2021-10-03 15:39:37 +0700

    add authentication controller scope

--- apps/todoapp-backend/src/task/task.controller.ts
+++ apps/todoapp-backend/src/task/task.controller.ts
@@ -6,14 +6,16 @@ import {
   Patch,
   Param,
   Delete,
-  Type,
+  UseGuards,
 } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { Types } from 'mongoose';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('task')
+@UseGuards(JwtAuthGuard)
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
--- apps/todoapp-backend/src/user/user.controller.ts
+++ apps/todoapp-backend/src/user/user.controller.ts
@@ -6,15 +6,20 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Public } from '../auth/auth.decorator';
 
 @Controller('user')
+@UseGuards(JwtAuthGuard)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Public()
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
